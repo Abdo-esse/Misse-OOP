68f518d2cff65042c82e87271ee5b81ee5a1748f
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-12-30 09:05:25 +0100

    j'ai temin la classe d'utilisateur

---
+++ classes/Utilisateur.php
@@ -0,0 +1,32 @@
+<?php
+
+class Utilisateur
+{
+   protected  $nom;
+   protected  $emaile;
+
+
+   public function __construct($nom,$emaile){
+    $this->nom=$nom;
+    $this->emaile=$emaile;
+   }
+   public function getNom(){
+    return $this->nom;
+   }
+   public function getEmaile(){
+    return $this->emaile;
+   }
+   public function setNom($nom){
+    $this->nom=$nom;
+   
+   }
+   public function setEmaile($emaile){
+    $this->emaile=$emaile;
+   }
+   public function __tostring(){
+    return "le nom est {$this->nom}, l'email est {$this->emaile} ";
+   }
+   
+
+
+}
